68736bf043263e68f7f61ca87e1f0d9ff730f786
Victor oc <ochando3d@gmail.com>
2021-05-31 00:41:11 +0100

    "first step in dockerizing emailSender"

---
+++ emailGenerator.ts
@@ -0,0 +1,29 @@
+//POR QUE TENGO QUE USAR FS2???? esto de que va junior
+const fs2 = require("fs");
+let bodyTemplate = fs2.readFileSync("./templates/newsBody.html").toString();
+
+function buildEmailBody(news, userTags) {
+    let newsConcat = "";
+    news.forEach((e) => {
+        let included = false;
+        let newsCategories = "";
+        e.tag.forEach((t) => {
+            if (userTags.includes(t.name)) {
+                included = true;
+            }
+            newsCategories = newsCategories + `[${t.name}] `;
+        });
+        if (included) {
+            let newsBodyTemporal = bodyTemplate
+                .replace("##$#categories#$##", newsCategories)
+                .replace("##$#newsTitle#$##", e.title)
+                .replace("##$#news#$##", e.body)
+                .replace("##$#newsUrl#$##", e.link);
+            newsConcat = newsConcat + newsBodyTemporal;
+        }
+    });
+
+    return newsConcat;
+}
+
+module.exports = buildEmailBody;
